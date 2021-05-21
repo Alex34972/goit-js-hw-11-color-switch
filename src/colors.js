965c2197b43d6f403a3c8027a19500764ea80094
Alexandr Petrenko <74877390+Alex34972@users.noreply.github.com>
2021-05-21 23:45:54 +0300

    Add files via upload

--- src/colors.js
+++ src/colors.js
@@ -1,14 +1,21 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-const btnStart = document.querySelector(`[data-action="start"]`)
-const btnStop = document.querySelector(`[data-action="stop"]`)
-
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+console.log(colors.length);
+const btnStart = document.querySelector(`[data-action="start"]`);
+const btnStop = document.querySelector(`[data-action="stop"]`);
+const body = document.querySelector(`body`);
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const onColoryzerBody = () => {
+  const i = randomIntegerFromInterval(0, colors.length);
+  body.style.backgroundColor = `${colors[i]}`;
+};
+btnStart.addEventListener(`click`, onColoryzerBody);
+//setInterval();
