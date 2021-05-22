dbf0483ffadad1115cd4f4891b2c2106902396f4
Alexandr Petrenko <74877390+Alex34972@users.noreply.github.com>
2021-05-22 13:15:54 +0300

    Add files via upload

--- src/colors.js
+++ src/colors.js
@@ -6,16 +6,25 @@ const colors = [
   "#009688",
   "#795548",
 ];
-console.log(colors.length);
 const btnStart = document.querySelector(`[data-action="start"]`);
 const btnStop = document.querySelector(`[data-action="stop"]`);
 const body = document.querySelector(`body`);
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-const onColoryzerBody = () => {
-  const i = randomIntegerFromInterval(0, colors.length);
-  body.style.backgroundColor = `${colors[i]}`;
-};
-btnStart.addEventListener(`click`, onColoryzerBody);
-//setInterval();
+let intervalId = null;
+btnStart.addEventListener(`click`, () => {
+  if (btnStart.dataset.action === "start") {
+    btnStart.removeAttribute("data-action");
+    intervalId = setInterval(() => {
+      const i = randomIntegerFromInterval(0, colors.length);
+      body.style.backgroundColor = `${colors[i]}`;
+    }, 1000);
+  }
+  return;
+});
+btnStart.removeAttribute("start");
+btnStop.addEventListener(`click`, () => {
+  clearTimeout(intervalId);
+  btnStart.setAttribute("data-action", "start");
+});
